3c9c724f2783e9bcfe91b6028e5ccad7d091c03b
Karina Tiurina <rktiurina@gmail.com>
2018-01-23 11:45:28 +0300

    destroy item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,45 @@
 class ItemsController < ApplicationController
-  before_action :set_repository, only: [:edit, :update, :destroy]
+  before_action :set_repository, only: [:new, :create, :destroy]
+  before_action :set_item, only: [:destroy]
 
+  def new
+    @new_item = @repository.items.build(params[:item])
+  end
 
+  def create
+    @new_item = @repository.items.build(item_params)
+
+    respond_to do |format|
+      if @new_item.save
+        format.html { redirect_to @repository, notice: 'Item was successfully uploaded.' }
+        format.json { render :show, status: :created, location: @repository }
+      else
+        format.html { render :new }
+        format.json { render json: @repository.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @item.destroy
+
+    respond_to do |format|
+      format.html { redirect_to repository_url(@repository), notice: 'Item was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def item_params
+      params.require(:item).permit(:name)
+  end
+
+  def set_repository
+    @repository = Repository.find(params[:repository_id])
+  end
+
+  def set_item
+    @item = @repository.items.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root 'repositories#index'
 
-  resources :repositories
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  resources :items
+  resources :repositories do
+    resources :items, only: [:new, :create, :destroy]
+  end
 end
