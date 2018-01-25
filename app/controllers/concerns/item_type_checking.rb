a83ee650ef282f87f06b527fa474122f37ece56b
Karina Tiurina <rktiurina@gmail.com>
2018-01-25 14:00:04 +0300

    include ItemsController to ItemScriptsController

--- app/controllers/concerns/item_type_checking.rb
+++ app/controllers/concerns/item_type_checking.rb
@@ -10,12 +10,12 @@ module ItemTypeChecking
   def self.build_new_item(controller_class_name, repository, request)
     return repository.item_files.build(request.params[:item_file]) if controller_class_name.to_s == 'ItemFilesController'
 
-    return repository.item_files.build(request.params[:item_script]) if controller_class_name.to_s == 'ItemScriptsController'
+    return repository.item_scripts.build(request.params[:item_script]) if controller_class_name.to_s == 'ItemScriptsController'
   end
 
   def self.build_to_create_item(controller_class_name, repository, item_params)
     return repository.item_files.build(item_params) if controller_class_name.to_s == 'ItemFilesController'
 
-    return repository.item_files.build(item_params) if controller_class_name.to_s == 'ItemScriptsController'
+    return repository.item_scripts.build(item_params) if controller_class_name.to_s == 'ItemScriptsController'
   end
 end
--- app/controllers/item_scripts_controller.rb
+++ app/controllers/item_scripts_controller.rb
@@ -1,49 +1,9 @@
 class ItemScriptsController < ApplicationController
-  before_action :set_repository, only: [:show, :new, :create, :destroy]
-  before_action :set_item_script, only: [:show, :destroy]
-
-  def show
-    @file_content = File.read("#{Rails.root}/public" + @item_script.source_file.to_s)
-  end
-
-  def new
-    @new_item_script = @repository.item_scripts.build(params[:item_script])
-  end
-
-  def create
-    @new_item_script = @repository.item_scripts.build(item_script_params)
-
-    respond_to do |format|
-      if @new_item_script.save
-        format.html { redirect_to @repository, notice: 'Script was successfully uploaded.' }
-        format.json { render :show, status: :created, location: @repository }
-      else
-        format.html { render :new }
-        format.json { render json: @repository.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @item_script.destroy
-
-    respond_to do |format|
-      format.html { redirect_to repository_url(@repository), notice: 'Item was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  include ItemsController
 
   private
 
   def item_params
       params.require(:item_script).permit(:name, :source_file, :command)
-  end
-
-  def set_repository
-    @repository = Repository.find(params[:repository_id])
-  end
-
-  def set_item_script
-    @item_script = @repository.item_scripts.find(params[:id])
   end
 end
