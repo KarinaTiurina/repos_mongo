50ffd4d8e84610925403e52494ae48947ddf5ce6
Karina Tiurina <rktiurina@gmail.com>
2018-01-30 09:57:23 +0300

    rspec: item concern

--- spec/models/item_script_spec.rb
+++ spec/models/item_script_spec.rb
@@ -1,23 +1,14 @@
 require 'rails_helper'
+require Rails.root.join "spec/concerns/item_spec.rb"
 
 RSpec.describe ItemScript, type: :model do
-  let(:test_repository) { FactoryGirl.create(:repository) }
-
-   let(:test_script) do
-     FactoryGirl.create(:item_script, repository: test_repository)
-  end
+  it_behaves_like "item"
 
   context 'document check' do
-    it { is_expected.to be_mongoid_document }
-    it { is_expected.to have_field(:name).of_type(String) }
-    it { is_expected.to have_field(:source_file).of_type(Object) }
     it { is_expected.to have_field(:command).of_type(String) }
-    it { is_expected.to be_embedded_in :repository }
   end
 
   context 'validation check' do
-    it { is_expected.to validate_presence_of :name }
-    it { is_expected.to validate_presence_of :source_file }
     it { is_expected.to validate_presence_of :command }
   end
 end
