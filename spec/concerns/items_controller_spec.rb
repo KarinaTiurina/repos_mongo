c8c968f912dcccb150caa010658a5e0033705c7b
Karina Tiurina <rktiurina@gmail.com>
2018-01-30 10:53:55 +0300

    rspec: items_controller concern

--- spec/concerns/items_controller_spec.rb
+++ spec/concerns/items_controller_spec.rb
@@ -4,14 +4,14 @@ shared_examples_for "items_controller" do
   let(:model) { described_class } # the class that includes the concern
 
   let(:test_repository) { FactoryBot.create(:repository) }
-  let(:test_item) { FactoryBot.create(:model) }
+  let(:test_item) { FactoryBot.create(model.name.underscore.gsub('s_controller','').to_sym) }
 
-  describe '#show' do
-    it 'show list' do
-      get :show, params: { id: test_item._id, repository_id: test_repository._id }
+  # describe '#show' do
+  #   it 'show list' do
+  #     get :show, params: { id: test_item._id, repository_id: test_repository._id }
 
-      expect(response.status).to eq(200)
-      expect(response).to render_template('show')
-    end
-  end
+  #     expect(response.status).to eq(200)
+  #     expect(response).to render_template('show')
+  #   end
+  # end
 end
