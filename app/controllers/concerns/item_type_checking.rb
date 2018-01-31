ce77ac2d60708683312923306a348974fc1b7706
Karina Tiurina <rktiurina@gmail.com>
2018-01-31 09:04:43 +0300

    ItemTypeChecking refactor

--- app/controllers/concerns/item_type_checking.rb
+++ app/controllers/concerns/item_type_checking.rb
@@ -2,20 +2,21 @@ module ItemTypeChecking
   extend ActiveSupport::Concern
 
   def self.set_item(controller_class_name, repository, request)
-    return repository.item_files.find(request.params[:id]) if controller_class_name.to_s == 'ItemFilesController'
-
-    return repository.item_scripts.find(request.params[:id]) if controller_class_name.to_s == 'ItemScriptsController'
+    return repository.method(items(controller_class_name)).call.find(request.params[:id])
   end
 
   def self.build_new_item(controller_class_name, repository, request)
-    return repository.item_files.build(request.params[:item_file]) if controller_class_name.to_s == 'ItemFilesController'
-
-    return repository.item_scripts.build(request.params[:item_script]) if controller_class_name.to_s == 'ItemScriptsController'
+    current_items = items(controller_class_name)
+    return repository.method(current_items).call.build(request.params[current_items.singularize.to_sym])
   end
 
   def self.build_to_create_item(controller_class_name, repository, item_params)
-    return repository.item_files.build(item_params) if controller_class_name.to_s == 'ItemFilesController'
+    return repository.method(items(controller_class_name)).call.build(item_params)
+  end
 
-    return repository.item_scripts.build(item_params) if controller_class_name.to_s == 'ItemScriptsController'
+  private
+
+  def self.items(controller_class_name)
+    controller_class_name.to_s.gsub("sController", '').tableize
   end
 end
